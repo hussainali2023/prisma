a0c15ce18d08425c5aafdb0f9d6cd4a12eb8a622
hussainali2023 <iamhussainali2@gmail.com>
2026-06-22 08:00:45 +0530

    added the middleware

--- blog_prisma_node/src/app.ts
+++ blog_prisma_node/src/app.ts
@@ -1,6 +1,12 @@
+import cookieParser from "cookie-parser"
 import express, { type Application, type Request, type Response } from "express"
-
+import cors from "cors"
 const app: Application = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
 
 app.get("/", (req: Request, res:Response) =>{
     res.send("Hello World")
