062bf125fd3b8bedb4e7d87907d2ffd212705fb6
hussainali2023 <iamhussainali2@gmail.com>
2026-06-30 22:19:03 +0530

    added token

--- blog_prisma_node/src/modules/users/user.route.ts
+++ blog_prisma_node/src/modules/users/user.route.ts
@@ -4,10 +4,22 @@ import status from "http-status";
 import { jwtUtils } from "../../utils/jwt";
 import config from "../../config";
 import { Role } from "../../../generated/prisma/enums";
+import catchAsync from "../../utils/catchAsync";
 
 const router = Router()
 
 router.post("/register", userController.createUser)
+
+const auth = () =>{
+    return catchAsync(async (req:Request, res:Response, next:NextFunction) =>{
+        const token = req.cookies.accessToken || req.headers.authorization?.startsWith("Bearer ") ? req.headers.authorization?.split(" ")[1] : req.headers.authorization
+
+        if(!token){
+            throw new Error("You are not logged in, please log in to access")
+        }
+    })
+}
+
 router.get("/me", (req:Request, res:Response, next: NextFunction)=> {
       const cookies = req.cookies;
       
